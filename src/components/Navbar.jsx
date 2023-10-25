16a8af6e04cad9d4aca805745952cf1919e0ce49
Kateattardo <youremail@domain.com>
2023-10-25 11:38:01 -0400

    nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f]"></div>
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+      {/* menu */}
+      <ul className=" hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+      {/* hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 w-full h-screen bg-blue-400 felx flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+
+      {/* social icons */}
+      <div className="hidden"></div>
+    </div>
   );
 };
 
