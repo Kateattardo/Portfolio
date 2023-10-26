5bcfc8dbc0dfc223023dd3f18f35f6830e92ced1
Kateattardo <youremail@domain.com>
2023-10-26 10:36:01 -0400

    homepage done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,8 +17,11 @@ const Home = () => {
           Here will be a short about me section.
         </p>
         <div>
-          <button className="text-white border-2 px-6 py-3 my-2 ">
-            View Work <HiArrowNarrowRight />
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
           </button>
         </div>
       </div>
