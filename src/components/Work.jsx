0b212432dacb72c19372dbc8c0c90ca891750b76
Kateattardo <youremail@domain.com>
2023-11-09 10:17:48 -0500

    font end code link

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -34,7 +34,7 @@ const Work = () => {
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a href="/https://github.com/Kateattardo/kitchwitchClient">
                   <button
                     className="text-center rounded-lg px-4 py-3 m-2
                        bg-white text-gray-700 font-bold text-lg"
